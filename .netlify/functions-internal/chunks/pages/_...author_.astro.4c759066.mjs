e8b86448f8c068cd0efa30969f42a77fc0f3d64c
Dmitry Mytnikau <kfifa3@gmail.com>
2023-05-24 22:46:21 +0300

    feat/ssr

---
+++ .netlify/functions-internal/chunks/pages/_...author_.astro.4c759066.mjs
@@ -0,0 +1,447 @@
+import 'kleur/colors';
+import 'node:fs/promises';
+import 'node:path';
+import 'node:url';
+import 'http-cache-semantics';
+import 'node:os';
+import 'image-size';
+import 'magic-string';
+import mime from 'mime';
+import 'node:stream';
+import { c as createAstro, a as createComponent, r as renderTemplate, m as maybeRenderHead, s as spreadAttributes, b as addAttribute, d as renderComponent } from '../astro.f03c80ea.mjs';
+import { s as slugify, f as formatDate, $ as $$Link, a as formatBlogPosts, b as $$MainLayout } from './404.astro.bbf30d01.mjs';
+
+function isOutputFormat(value) {
+  return ["avif", "jpeg", "jpg", "png", "webp", "svg"].includes(value);
+}
+function isOutputFormatSupportsAlpha(value) {
+  return ["avif", "png", "webp"].includes(value);
+}
+function isAspectRatioString(value) {
+  return /^\d*:\d*$/.test(value);
+}
+function parseAspectRatio(aspectRatio) {
+  if (!aspectRatio) {
+    return void 0;
+  }
+  if (typeof aspectRatio === "number") {
+    return aspectRatio;
+  } else {
+    const [width, height] = aspectRatio.split(":");
+    return parseInt(width) / parseInt(height);
+  }
+}
+function isSSRService(service) {
+  return "transform" in service;
+}
+class BaseSSRService {
+  async getImageAttributes(transform) {
+    const { width, height, src, format, quality, aspectRatio, ...rest } = transform;
+    return {
+      ...rest,
+      width,
+      height
+    };
+  }
+  serializeTransform(transform) {
+    const searchParams = new URLSearchParams();
+    if (transform.quality) {
+      searchParams.append("q", transform.quality.toString());
+    }
+    if (transform.format) {
+      searchParams.append("f", transform.format);
+    }
+    if (transform.width) {
+      searchParams.append("w", transform.width.toString());
+    }
+    if (transform.height) {
+      searchParams.append("h", transform.height.toString());
+    }
+    if (transform.aspectRatio) {
+      searchParams.append("ar", transform.aspectRatio.toString());
+    }
+    if (transform.fit) {
+      searchParams.append("fit", transform.fit);
+    }
+    if (transform.background) {
+      searchParams.append("bg", transform.background);
+    }
+    if (transform.position) {
+      searchParams.append("p", encodeURI(transform.position));
+    }
+    searchParams.append("href", transform.src);
+    return { searchParams };
+  }
+  parseTransform(searchParams) {
+    if (!searchParams.has("href")) {
+      return void 0;
+    }
+    let transform = { src: searchParams.get("href") };
+    if (searchParams.has("q")) {
+      transform.quality = parseInt(searchParams.get("q"));
+    }
+    if (searchParams.has("f")) {
+      const format = searchParams.get("f");
+      if (isOutputFormat(format)) {
+        transform.format = format;
+      }
+    }
+    if (searchParams.has("w")) {
+      transform.width = parseInt(searchParams.get("w"));
+    }
+    if (searchParams.has("h")) {
+      transform.height = parseInt(searchParams.get("h"));
+    }
+    if (searchParams.has("ar")) {
+      const ratio = searchParams.get("ar");
+      if (isAspectRatioString(ratio)) {
+        transform.aspectRatio = ratio;
+      } else {
+        transform.aspectRatio = parseFloat(ratio);
+      }
+    }
+    if (searchParams.has("fit")) {
+      transform.fit = searchParams.get("fit");
+    }
+    if (searchParams.has("p")) {
+      transform.position = decodeURI(searchParams.get("p"));
+    }
+    if (searchParams.has("bg")) {
+      transform.background = searchParams.get("bg");
+    }
+    return transform;
+  }
+}
+
+function isRemoteImage(src) {
+  return /^(https?:)?\/\//.test(src);
+}
+function removeQueryString(src) {
+  const index = src.lastIndexOf("?");
+  return index > 0 ? src.substring(0, index) : src;
+}
+function extname(src) {
+  const base = basename(src);
+  const index = base.lastIndexOf(".");
+  if (index <= 0) {
+    return "";
+  }
+  return base.substring(index);
+}
+function basename(src) {
+  return removeQueryString(src.replace(/^.*[\\\/]/, ""));
+}
+
+function resolveSize(transform) {
+  if (transform.width && transform.height) {
+    return transform;
+  }
+  if (!transform.width && !transform.height) {
+    throw new Error(`"width" and "height" cannot both be undefined`);
+  }
+  if (!transform.aspectRatio) {
+    throw new Error(
+      `"aspectRatio" must be included if only "${transform.width ? "width" : "height"}" is provided`
+    );
+  }
+  let aspectRatio;
+  if (typeof transform.aspectRatio === "number") {
+    aspectRatio = transform.aspectRatio;
+  } else {
+    const [width, height] = transform.aspectRatio.split(":");
+    aspectRatio = Number.parseInt(width) / Number.parseInt(height);
+  }
+  if (transform.width) {
+    return {
+      ...transform,
+      width: transform.width,
+      height: Math.round(transform.width / aspectRatio)
+    };
+  } else if (transform.height) {
+    return {
+      ...transform,
+      width: Math.round(transform.height * aspectRatio),
+      height: transform.height
+    };
+  }
+  return transform;
+}
+async function resolveTransform(input) {
+  if (typeof input.src === "string") {
+    return resolveSize(input);
+  }
+  const metadata = "then" in input.src ? (await input.src).default : input.src;
+  let { width, height, aspectRatio, background, format = metadata.format, ...rest } = input;
+  if (!width && !height) {
+    width = metadata.width;
+    height = metadata.height;
+  } else if (width) {
+    let ratio = parseAspectRatio(aspectRatio) || metadata.width / metadata.height;
+    height = height || Math.round(width / ratio);
+  } else if (height) {
+    let ratio = parseAspectRatio(aspectRatio) || metadata.width / metadata.height;
+    width = width || Math.round(height * ratio);
+  }
+  return {
+    ...rest,
+    src: metadata.src,
+    width,
+    height,
+    aspectRatio,
+    format,
+    background
+  };
+}
+async function getImage(transform) {
+  var _a, _b, _c;
+  if (!transform.src) {
+    throw new Error("[@astrojs/image] `src` is required");
+  }
+  let loader = (_a = globalThis.astroImage) == null ? void 0 : _a.loader;
+  if (!loader) {
+    const { default: mod } = await import('./endpoint.js.30cc23b9.mjs').then(n => n.s).catch(() => {
+      throw new Error(
+        "[@astrojs/image] Builtin image loader not found. (Did you remember to add the integration to your Astro config?)"
+      );
+    });
+    loader = mod;
+    globalThis.astroImage = globalThis.astroImage || {};
+    globalThis.astroImage.loader = loader;
+  }
+  const resolved = await resolveTransform(transform);
+  const attributes = await loader.getImageAttributes(resolved);
+  const isDev = (_b = (Object.assign({"BASE_URL":"/","MODE":"production","DEV":false,"PROD":true,"SSR":true,"SITE":"https://astro-tehnocats.netlify.app/","ASSETS_PREFIX":undefined},{SSR:true,}))) == null ? void 0 : _b.DEV;
+  const isLocalImage = !isRemoteImage(resolved.src);
+  const _loader = isDev && isLocalImage ? globalThis.astroImage.defaultLoader : loader;
+  if (!_loader) {
+    throw new Error("@astrojs/image: loader not found!");
+  }
+  const { searchParams } = isSSRService(_loader) ? _loader.serializeTransform(resolved) : globalThis.astroImage.defaultLoader.serializeTransform(resolved);
+  const imgSrc = !isLocalImage && resolved.src.startsWith("//") ? `https:${resolved.src}` : resolved.src;
+  let src;
+  if (/^[\/\\]?@astroimage/.test(imgSrc)) {
+    src = `${imgSrc}?${searchParams.toString()}`;
+  } else {
+    searchParams.set("href", imgSrc);
+    src = `/_image?${searchParams.toString()}`;
+  }
+  if ((_c = globalThis.astroImage) == null ? void 0 : _c.addStaticImage) {
+    src = globalThis.astroImage.addStaticImage(resolved);
+  }
+  return {
+    ...attributes,
+    src
+  };
+}
+
+async function resolveAspectRatio({ src, aspectRatio }) {
+  if (typeof src === "string") {
+    return parseAspectRatio(aspectRatio);
+  } else {
+    const metadata = "then" in src ? (await src).default : src;
+    return parseAspectRatio(aspectRatio) || metadata.width / metadata.height;
+  }
+}
+async function resolveFormats({ src, formats }) {
+  const unique = new Set(formats);
+  if (typeof src === "string") {
+    unique.add(extname(src).replace(".", ""));
+  } else {
+    const metadata = "then" in src ? (await src).default : src;
+    unique.add(extname(metadata.src).replace(".", ""));
+  }
+  return Array.from(unique).filter(Boolean);
+}
+async function getPicture(params) {
+  const { src, alt, widths, fit, position, background } = params;
+  if (!src) {
+    throw new Error("[@astrojs/image] `src` is required");
+  }
+  if (!widths || !Array.isArray(widths)) {
+    throw new Error("[@astrojs/image] at least one `width` is required. ex: `widths={[100]}`");
+  }
+  const aspectRatio = await resolveAspectRatio(params);
+  if (!aspectRatio) {
+    throw new Error("`aspectRatio` must be provided for remote images");
+  }
+  const allFormats = await resolveFormats(params);
+  const lastFormat = allFormats[allFormats.length - 1];
+  const maxWidth = Math.max(...widths);
+  let image;
+  async function getSource(format) {
+    const imgs = await Promise.all(
+      widths.map(async (width) => {
+        const img = await getImage({
+          src,
+          alt,
+          format,
+          width,
+          fit,
+          position,
+          background,
+          aspectRatio
+        });
+        if (format === lastFormat && width === maxWidth) {
+          image = img;
+        }
+        return `${img.src} ${width}w`;
+      })
+    );
+    return {
+      type: mime.getType(format) || format,
+      srcset: imgs.join(",")
+    };
+  }
+  const sources = await Promise.all(allFormats.map((format) => getSource(format)));
+  return {
+    sources,
+    // @ts-expect-error image will always be defined
+    image
+  };
+}
+
+const $$Astro$3 = createAstro("https://astro-tehnocats.netlify.app/");
+const $$Image = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro$3, $$props, $$slots);
+  Astro2.self = $$Image;
+  const { loading = "lazy", decoding = "async", ...props } = Astro2.props;
+  if (props.alt === void 0 || props.alt === null) {
+    warnForMissingAlt();
+  }
+  const attrs = await getImage(props);
+  return renderTemplate`${maybeRenderHead($$result)}<img${spreadAttributes(attrs)}${addAttribute(loading, "loading")}${addAttribute(decoding, "decoding")}>`;
+}, "D:/webWork/intern/projects/birka/node_modules/@astrojs/image/components/Image.astro");
+
+const $$Astro$2 = createAstro("https://astro-tehnocats.netlify.app/");
+const $$Picture = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro$2, $$props, $$slots);
+  Astro2.self = $$Picture;
+  const {
+    src,
+    alt,
+    sizes,
+    widths,
+    aspectRatio,
+    fit,
+    background,
+    position,
+    formats = ["avif", "webp"],
+    loading = "lazy",
+    decoding = "async",
+    ...attrs
+  } = Astro2.props;
+  if (alt === void 0 || alt === null) {
+    warnForMissingAlt();
+  }
+  const { image, sources } = await getPicture({
+    src,
+    widths,
+    formats,
+    aspectRatio,
+    fit,
+    background,
+    position,
+    alt
+  });
+  delete image.width;
+  delete image.height;
+  return renderTemplate`${maybeRenderHead($$result)}<picture>
+	${sources.map((attrs2) => renderTemplate`<source${spreadAttributes(attrs2)}${addAttribute(sizes, "sizes")}>`)}
+	<img${spreadAttributes(image)}${addAttribute(loading, "loading")}${addAttribute(decoding, "decoding")}${spreadAttributes(attrs)}>
+</picture>`;
+}, "D:/webWork/intern/projects/birka/node_modules/@astrojs/image/components/Picture.astro");
+
+let altWarningShown = false;
+function warnForMissingAlt() {
+  if (altWarningShown === true) {
+    return;
+  }
+  altWarningShown = true;
+  console.warn(`
+[@astrojs/image] "alt" text was not provided for an <Image> or <Picture> component.
+
+A future release of @astrojs/image may throw a build error when "alt" text is missing.
+
+The "alt" attribute holds a text description of the image, which isn't mandatory but is incredibly useful for accessibility. Set to an empty string (alt="") if the image is not a key part of the content (it's decoration or a tracking pixel).
+`);
+}
+
+const $$Astro$1 = createAstro("https://astro-tehnocats.netlify.app/");
+const $$PostCard = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro$1, $$props, $$slots);
+  Astro2.self = $$PostCard;
+  const { frontmatter, url, tagType } = Astro2.props;
+  const { title, date, category, author, image, description } = frontmatter;
+  const Tag = tagType;
+  return renderTemplate`${maybeRenderHead($$result)}<article class="card">
+    <small>
+        <a${addAttribute(`/category/${slugify(category)}/`, "href")} class="badge" }>${category}</a>
+    </small>
+    <a${addAttribute(url, "href")} aria-hidden="true" tabindex="-1">
+        ${renderComponent($$result, "Image", $$Image, { "src": image.src, "alt": image.alt, "width": "400", "height": "250", "format": "avif", "fit": "cover", "quality": 80, "aspectRatio": "4:1" })}
+    </a>
+    <div class="content">
+        <div>
+            ${renderComponent($$result, "Tag", Tag, { "class": "h3" }, { "default": ($$result2) => renderTemplate`
+                <a${addAttribute(url, "href")}>${title}</a>
+            ` })}
+            <small>
+                by
+                <a${addAttribute(`/author/${slugify(author)}/`, "href")}>${author}
+                </a> - ${formatDate(date)}
+            </small>
+        </div>
+        <p>${description}</p>
+        ${renderComponent($$result, "Link", $$Link, { "href": url, "text": "Read post", "style": "secondary" })}
+    </div>   
+</article>`;
+}, "D:/webWork/intern/projects/birka/src/components/PostCard.astro");
+
+const $$Astro = createAstro("https://astro-tehnocats.netlify.app/");
+const Astro = $$Astro;
+async function getStaticPaths() {
+  const allPosts = await Astro.glob(/* #__PURE__ */ Object.assign({"../blog/aleksei-smirnov.md": () => import('./aleksei-smirnov.md.3366715d.mjs').then(n => n._),"../blog/alexander-semenov.md": () => import('./alexander-semenov.md.bf9941b1.mjs').then(n => n._),"../blog/dmitry-mytnikau.md": () => import('./dmitry-mytnikau.md.cecad0c7.mjs').then(n => n._),"../blog/elizaveta-obrezkova.md": () => import('./elizaveta-obrezkova.md.10467632.mjs').then(n => n._),"../blog/maria-kikot.md": () => import('./maria-kikot.md.b81238e8.mjs').then(n => n._)}), () => "../blog/*.md");
+  const formattedPosts = formatBlogPosts(allPosts);
+  const allAuthors = [
+    ...new Set(
+      formattedPosts.map((post) => post.frontmatter.author).flat()
+    )
+  ];
+  return allAuthors.map((author) => {
+    const authorPosts = formattedPosts.filter((post) => post.frontmatter.author === author);
+    return {
+      params: { author: slugify(author) },
+      props: {
+        authorName: author,
+        posts: authorPosts
+      }
+    };
+  });
+}
+const $$ = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro, $$props, $$slots);
+  Astro2.self = $$;
+  Astro2.params;
+  const { posts, authorName } = Astro2.props;
+  return renderTemplate`${renderComponent($$result, "MainLayout", $$MainLayout, { "title": authorName }, { "default": ($$result2) => renderTemplate`
+  ${maybeRenderHead($$result2)}<section class="container"${addAttribute(`Posts by ${authorName}`, "aria-label")}>
+      <h1 class="h1">Posts by <span>${authorName}</span></h1>
+      <div class="post-container">
+        ${posts.map((post) => renderTemplate`${renderComponent($$result2, "PostCard", $$PostCard, { "frontmatter": post.frontmatter, "url": post.url, "tagType": "h2" })}`)}
+      </div>
+  </section>
+` })}`;
+}, "D:/webWork/intern/projects/birka/src/pages/author/[...author].astro");
+
+const $$file = "D:/webWork/intern/projects/birka/src/pages/author/[...author].astro";
+const $$url = "/author/[...author]";
+
+const ____author_ = /*#__PURE__*/Object.freeze(/*#__PURE__*/Object.defineProperty({
+  __proto__: null,
+  default: $$,
+  file: $$file,
+  getStaticPaths,
+  url: $$url
+}, Symbol.toStringTag, { value: 'Module' }));
+
+export { $$PostCard as $, BaseSSRService as B, ____author_ as _, $$Image as a, isRemoteImage as b, isOutputFormatSupportsAlpha as i };
